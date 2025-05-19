d6b37fd46f3bfaa41f34c08266a8596b2202ebee
Annette04 <rebakad@gmail.com>
2025-05-19 17:14:08 +0500

    add views

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -1,1 +1,39 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    author_name = request.GET.get('author_name', '')
+    if author_name:
+        tasks = Task.objects.filter(author_name=author_name)
+    else:
+        tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks, 'author_name': author_name})
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+def task_update(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
