f99299283538cf81ceeb1f8e16c575a9b13915c7
Annette04 <rebakad@gmail.com>
2025-05-22 16:54:57 +0500

    add upload photo

--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -12,7 +12,7 @@ def task_list(request):
 
 def task_create(request):
     if request.method == 'POST':
-        form = TaskForm(request.POST)
+        form = TaskForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('task_list')
@@ -23,7 +23,7 @@ def task_create(request):
 def task_update(request, pk):
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
-        form = TaskForm(request.POST, instance=task)
+        form = TaskForm(request.POST, request.FILES, instance=task)
         if form.is_valid():
             form.save()
             return redirect('task_list')
