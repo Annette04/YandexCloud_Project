91ece15b71a871870a7dfb5b4c4b6bb48c9f5676
Almazova Vlada <almazova.2004@mail.ru>
2025-05-23 11:54:17 +0500

    add required_providers network

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.140"
+    }
+  }
+}
+
 resource "yandex_vpc_network" "network" {
   name = var.network_name
 }
