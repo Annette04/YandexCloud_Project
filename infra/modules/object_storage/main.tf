ece1fb8d4caac6fbe88c808c8f699b3310e25809
Almazova Vlada <almazova.2004@mail.ru>
2025-05-26 01:47:30 +0500

    correct iam_binding

--- infra/modules/object_storage/main.tf
+++ infra/modules/object_storage/main.tf
@@ -12,20 +12,11 @@ resource "yandex_storage_bucket" "images" {
   acl    = "private"
 }
 
-# Доступ cloud_function_sa
-resource "yandex_resourcemanager_folder_iam_member" "cloud_function_sa_access" {
-  for_each = toset(var.cloud_function_sa_roles)
-
-  folder_id = var.folder_id
-  role      = each.value
-  member    = "serviceAccount:${var.cloud_function_sa_id}"
-}
-
-# Доступ app_server_sa
-resource "yandex_resourcemanager_folder_iam_member" "app_server_sa_access" {
-  for_each = toset(var.app_server_sa_roles)
-
-  folder_id = var.folder_id
-  role      = each.value
-  member    = "serviceAccount:${var.app_server_sa_id}"
+resource "yandex_storage_bucket_iam_binding" "editor" {
+  bucket = yandex_storage_bucket.images.bucket
+  role   = "storage.editor"
+  members = [
+    "serviceAccount:${module.app_server_sa.service_account_id}",
+    "serviceAccount:${module.cloud_function_sa.service_account_id}"
+  ]
 }
