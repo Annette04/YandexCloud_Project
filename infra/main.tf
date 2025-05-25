488ac048dacfdcd711957ad83c5057cd279f3517
Almazova Vlada <almazova.2004@mail.ru>
2025-05-25 16:20:41 +0500

    add roles

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,30 @@ module "network" {
   source       = "./modules/network"
   network_name = "notes-network"
 }
+
+module "admin_sa" {
+  source               = "./modules/iam"
+  folder_id            = var.yc_folder_id
+  service_account_name = "admin-sa"
+  roles                = ["editor"]
+}
+
+module "app_server_sa" {
+  source               = "./modules/iam"
+  folder_id            = var.yc_folder_id
+  service_account_name = "app-server-sa"
+  roles                = [
+    "storage.editor",
+    "mdb.admin",
+    "serverless.functions.invoker"
+  ]
+}
+
+module "cloud_function_sa" {
+  source               = "./modules/iam"
+  folder_id            = var.yc_folder_id
+  service_account_name = "cloud-function-sa"
+  roles                = [
+    "storage.editor"
+  ]
+}
---
+++ infra/modules/iam/main.tf
@@ -0,0 +1,10 @@
+resource "yandex_iam_service_account" "this" {
+  name = var.service_account_name
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "roles" {
+  for_each = toset(var.roles)
+  folder_id = var.folder_id
+  role      = each.value
+  member    = "serviceAccount:${yandex_iam_service_account.this.id}"
+}
