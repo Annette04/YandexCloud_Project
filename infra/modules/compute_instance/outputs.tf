f25d4f2a02349ea4d4b04c22faf317f3a24b52bb
Almazova Vlada <almazova.2004@mail.ru>
2025-05-30 03:15:07 +0500

    del vm

--- infra/modules/compute_instance/outputs.tf
+++ infra/modules/compute_instance/outputs.tf
@@ -1,7 +1,7 @@
-output "vm_id" {
-  value = yandex_compute_instance.default.id
-}
-
-output "external_ip" {
-  value = try(yandex_compute_instance.default.network_interface[0].nat_ip_address, "")
-}
+#output "vm_id" {
+#  value = yandex_compute_instance.default.id
+#}
+#
+#output "external_ip" {
+#  value = try(yandex_compute_instance.default.network_interface[0].nat_ip_address, "")
+#}
