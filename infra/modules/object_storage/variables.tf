9ace55cb70cfb3a0071b203ad3fb1f9fe21b2594
Annette04 <rebakad@gmail.com>
2025-05-29 23:43:54 +0500

    fix problems 3

--- infra/modules/object_storage/variables.tf
+++ infra/modules/object_storage/variables.tf
@@ -5,3 +5,8 @@ variable "folder_id" {
 variable "bucket_name" {
   type = string
 }
+
+variable "service_account_id" {
+  description = "Service Account ID for the function"
+  type        = string
+}
