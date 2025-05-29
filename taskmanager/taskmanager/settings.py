40937f7b606e9acf05a3f59d8d6475fdc39bed19
Almazova Vlada <almazova.2004@mail.ru>
2025-05-30 03:52:57 +0500

    update settings

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#j3*wg0#w@y6c_h02@pbqxbm31859z8!gps662^*=$c&f%(qbm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['51.250.76.128']
 
 
 # Application definition
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '6432'),
     }
 }
 
