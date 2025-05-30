15506684ad11573b88fed3f7c0897eb2271a9273
Annette04 <rebakad@gmail.com>
2025-05-30 06:11:18 +0500

    fix main in load_balancer

--- infra/modules/load_balancer/main.tf
+++ infra/modules/load_balancer/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "~> 0.140"
+    }
+  }
+}
+
 resource "yandex_lb_network_load_balancer" "main" {
   name = var.lb_name
 
