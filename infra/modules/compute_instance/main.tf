42e8e6feefb3e3d365708e44f9ed3624a0045756
Almazova Vlada <almazova.2004@mail.ru>
2025-06-09 21:28:48 +0500

    add VM

--- infra/modules/compute_instance/main.tf
+++ infra/modules/compute_instance/main.tf
@@ -1,41 +1,41 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source = "yandex-cloud/yandex"
-#    }
-#  }
-#}
-#
-#resource "yandex_compute_instance" "default" {
-#  name        = var.vm_name
-#  folder_id   = var.folder_id
-#  zone        = var.zone
-#  service_account_id = var.service_account_id
-#
-#  resources {
-#    cores         = var.cpu
-#    memory        = var.memory
-#    core_fraction = 100
-#  }
-#
-#  boot_disk {
-#    initialize_params {
-#      image_id = data.yandex_compute_image.ubuntu.id
-#      size     = var.disk_size
-#    }
-#  }
-#
-#  network_interface {
-#    subnet_id = var.subnet_id
-#    nat       = var.public_ip ? true : false
-#  }
-#
-#  metadata = {
-#    ssh-keys = "ubuntu:${var.ssh_key}"
-#    serial-port-enable = "1"
-#  }
-#}
-#
-#data "yandex_compute_image" "ubuntu" {
-#  family = var.image_family
-#}
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+resource "yandex_compute_instance" "default" {
+  name        = var.vm_name
+  folder_id   = var.folder_id
+  zone        = var.zone
+  service_account_id = var.service_account_id
+
+  resources {
+    cores         = var.cpu
+    memory        = var.memory
+    core_fraction = 100
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.id
+      size     = var.disk_size
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = var.public_ip ? true : false
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${var.ssh_key}"
+    serial-port-enable = "1"
+  }
+}
+
+data "yandex_compute_image" "ubuntu" {
+  family = var.image_family
+}
