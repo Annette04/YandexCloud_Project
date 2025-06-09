d27c207b789dd4994850f0954df078ae0a9c675f
Almazova Vlada <almazova.2004@mail.ru>
2025-06-09 15:36:05 +0500

    del db

--- infra/modules/postgresql/outputs.tf
+++ infra/modules/postgresql/outputs.tf
@@ -1,24 +1,24 @@
-output "notes_db_cluster_id" {
-  description = "ID кластера PostgreSQL"
-  value       = yandex_mdb_postgresql_cluster.notes_db_cluster.id
-}
-
-output "notes_db_cluster_name" {
-  description = "Имя кластера PostgreSQL"
-  value       = yandex_mdb_postgresql_cluster.notes_db_cluster.name
-}
-
-output "notes_db_name" {
-  description = "Имя базы данных"
-  value       = yandex_mdb_postgresql_database.notes_db.name
-}
-
-output "notes_db_user_name" {
-  description = "Имя пользователя базы данных"
-  value       = yandex_mdb_postgresql_user.django_user.name
-}
-
-output "notes_db_host_fqdns" {
-  description = "FQDN хостов в кластере"
-  value       = [for host in yandex_mdb_postgresql_cluster.notes_db_cluster.host : host.fqdn]
-}
+#output "notes_db_cluster_id" {
+#  description = "ID кластера PostgreSQL"
+#  value       = yandex_mdb_postgresql_cluster.notes_db_cluster.id
+#}
+#
+#output "notes_db_cluster_name" {
+#  description = "Имя кластера PostgreSQL"
+#  value       = yandex_mdb_postgresql_cluster.notes_db_cluster.name
+#}
+#
+#output "notes_db_name" {
+#  description = "Имя базы данных"
+#  value       = yandex_mdb_postgresql_database.notes_db.name
+#}
+#
+#output "notes_db_user_name" {
+#  description = "Имя пользователя базы данных"
+#  value       = yandex_mdb_postgresql_user.django_user.name
+#}
+#
+#output "notes_db_host_fqdns" {
+#  description = "FQDN хостов в кластере"
+#  value       = [for host in yandex_mdb_postgresql_cluster.notes_db_cluster.host : host.fqdn]
+#}
