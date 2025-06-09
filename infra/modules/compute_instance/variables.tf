e8aa1b3e0c2f35746192082e905fb2468ac28883
Almazova Vlada <almazova.2004@mail.ru>
2025-06-09 21:27:08 +0500

    del VM

--- infra/modules/compute_instance/variables.tf
+++ infra/modules/compute_instance/variables.tf
@@ -1,23 +1,23 @@
-variable "folder_id" {}
-variable "subnet_id" {}
-variable "service_account_id" {}
-variable "vm_name" {}
-variable "zone" {
-  default = "ru-central1-a"
-}
-variable "ssh_key" {}
-variable "image_family" {
-  default = "ubuntu-2204-lts"
-}
-variable "cpu" {
-  default = 2
-}
-variable "memory" {
-  default = 4
-}
-variable "disk_size" {
-  default = 20
-}
-variable "public_ip" {
-  default = true
-}
+#variable "folder_id" {}
+#variable "subnet_id" {}
+#variable "service_account_id" {}
+#variable "vm_name" {}
+#variable "zone" {
+#  default = "ru-central1-a"
+#}
+#variable "ssh_key" {}
+#variable "image_family" {
+#  default = "ubuntu-2204-lts"
+#}
+#variable "cpu" {
+#  default = 2
+#}
+#variable "memory" {
+#  default = 4
+#}
+#variable "disk_size" {
+#  default = 20
+#}
+#variable "public_ip" {
+#  default = true
+#}
